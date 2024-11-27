1dd9813fb99eee4635d4bec45305c96c0cb67aa5
douglasliao77 <douglasliao77@gmail.com>
2024-11-27 14:01:36 +0100

    finihsed with the code tasks

--- Wikipedia/wikipedia.py
+++ Wikipedia/wikipedia.py
@@ -5,7 +5,6 @@ from scipy import linalg
 from matplotlib import pyplot as plt
 import pandas as pd
 
-
 """
 Read nodes and edges
 """
@@ -13,7 +12,6 @@ def read(file):
     nodes = open("titles/"+file, "r").read().strip().splitlines()
     edges = np.loadtxt("links/"+file, dtype=int)
     return nodes, edges
-
 
 """
 Create a adjacency matrix given the edges and nr of nodes
@@ -34,7 +32,7 @@ def get_degree(A,n):
     k_in = np.dot(A, u)
     k_out = np.dot(A.T,u)
 
-    # Normalize 
+    # Normalize the vector
     k_in /= np.sum(k_in)
     k_out /= np.sum(k_out) 
 
@@ -43,7 +41,7 @@ def get_degree(A,n):
 if __name__ == "__main__": 
     pd.set_option('display.float_format', '{:.6f}'.format)
     # Read data 
-    file = "2.txt"
+    file = "1.txt"
     nodes, edges = read(file)
     nr_nodes = len(nodes)
     A = getA(edges, nr_nodes)
@@ -96,10 +94,15 @@ if __name__ == "__main__":
     print(df4.to_string(index=False))
     
     print("--------------Task 3------------")
-
+    N = A.shape[0]
     eig_val, eig_vec = np.linalg.eig(A)
     idx = np.argmax(eig_val.real)
-    eig_centrality = eig_vec.real[:,idx] / np.sum(np.abs(eig_vec.real[:,idx]))
+    # eig_centrality = eig_vec.real[:,idx] / np.sum(eig_vec.real[:,idx])
+    # top_eig_indices = np.argsort(eig_centrality)[-5:]
+
+    alpha = 1/eig_val[idx]
+    eig_centrality = np.power((alpha * A), 150) @ np.ones((N,))
+    eig_centrality = eig_centrality.real / np.sum(eig_centrality.real)
     top_eig_indices = np.argsort(eig_centrality)[-5:]
 
     df5 = pd.DataFrame({
@@ -108,4 +111,104 @@ if __name__ == "__main__":
     })
     print(df5.to_string(index=False))
 
+    print("--------------Task 4------------")
+    lambda_max = np.abs(eig_val[idx])
+    alpha = 0.85 / lambda_max 
+
+    N = A.shape[0] # eigenvector size
+    I = np.eye(N) # Identity matrix
+    U = np.ones(N) # Vector of ones
+
+    # Ingoing influence - based on the number and importance of nodes pointing
+    katz_centrality = (1/N)*np.linalg.inv(I - alpha * A).dot(U) 
+    katz_centrality = katz_centrality / np.sum(katz_centrality)
+    top_katz_indices = np.argsort(katz_centrality)[-5:]
+
+    df6 = pd.DataFrame({
+        "Top Katz": nodes[top_katz_indices][::-1],
+        "Katz centrality": katz_centrality[top_katz_indices][::-1],
+    })
+
+    print(df6.to_string(index=False))
+    print("--------------Task 5------------")
+    N = A.shape[0]
+    alphas = [0.3, 0.99, 0.85]
+
+    H = A / np.sum(A, axis=0)  
+    H[:, np.sum(A, axis=0) == 0] = 1 / N # Nodes with outgoing links set to 1/N
+
+    I = np.eye(N)
+    U = np.ones(N)
+    _PR = 0 # Used for the comparison in the next task
+    for alpha in alphas:
+        PR = ((1 - alpha) / N) * np.linalg.inv(I - alpha * H) @ U
+        PR = PR / np.sum(PR)
+        top_pr_indices = np.argsort(PR)[-5:]
+        if alpha == 0.85:
+            _PR = PR
+
+        df7 = pd.DataFrame({
+            f"Top PageRank, alpha={alpha}": nodes[top_pr_indices][::-1],
+            "PageRank": PR[top_pr_indices][::-1],
+        })
+        print(df7.to_string(index=False))
+        print("-"*50)
+
+    print("--------------Task 6------------")
+    top_three = top_pr_indices[::-1][:3]
+    print("Top-three articles are: " + str(top_three))
+
+
+    alpha = 0.85
+    G = alpha*H + ((1 - alpha) / N) * np.ones((N,N))
+    r = np.ones(N) / N
     
+    history = np.zeros((101, N))
+    history[0] = r
+    for i in range(100):
+        r = G @ r
+        r = r / np.sum(r)
+        history[i+1] = r
+        if i+1 in [1,2,5,10,100]:
+            top_pr_indices = np.argsort(r)[-5:]
+            df8 = pd.DataFrame({
+                f"Top iterative PageRank, alpha={alpha}": nodes[top_pr_indices][::-1],
+                f"PageRank, iteration {i+1}": r[top_pr_indices][::-1],
+            })
+            print(df8.to_string(index=False))
+            print("-"*50)
+
+    # Plot the centrality of top three articles in the iterative method
+    plt.figure(1)
+    for idx in top_three:
+        plt.hlines(
+            _PR[idx],
+            colors='green',
+            xmin=0, xmax=100, 
+            label=f"PageRank task 5")
+
+    plt.plot(
+        range(101), 
+        history[:, top_three[0]], 
+        'k--' ,
+        label=f"{nodes[top_three[0]]}",
+    )
+    plt.plot(
+        range(101), 
+        history[:, top_three[1]], 
+        'b--' ,
+        label=f"{nodes[top_three[1]]}",
+    )
+    plt.plot(
+        range(101), 
+        history[:, top_three[2]], 
+        'r--' ,
+        label=f"{nodes[top_three[2]]}",
+    )
+    
+    plt.xlabel('Iterations')
+    plt.ylabel('PageRank')
+    plt.title('Top 3 Articles')
+    plt.legend()
+    plt.show()
+
